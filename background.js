2c4e04ca79e1de3f0efdee265773ce6182150515
Rob Hoelz <rob@hoelz.ro>
2019-06-03 22:26:19 -0500

    Store page ID in key along with comment ID
    This allows me to easily cluster comments by the page on which they live

--- background.js
+++ background.js
@@ -1,8 +1,19 @@
 const HN_PAGE_PATTERN = '*://news.ycombinator.com/item?id=*';
 const HN_COMMENT_PATTERN = HN_PAGE_PATTERN; // happens to be the same
-const HN_COMMENT_RE = new RegExp('^http[s]?://news[.]ycombinator[.]com/item[?]id=(\\d+)$');
+const HN_PAGE_RE = new RegExp('^http[s]?://news[.]ycombinator[.]com/item[?]id=(\\d+)$');
+const HN_COMMENT_RE = HN_PAGE_RE; // happens to be the same
 
 const STORAGE = browser.storage.local;
+
+function extractPageId(url) {
+    let match = HN_PAGE_RE.exec(url);
+
+    if(match) {
+        return match[1];
+    }
+
+    return null;
+}
 
 function extractCommentId(url) {
     let match = HN_COMMENT_RE.exec(url);
@@ -14,21 +25,20 @@ function extractCommentId(url) {
     return null;
 }
 
-async function isMarked(id) {
-    // XXX use URL of page itself in the key?
-    let results = await STORAGE.get(id);
+async function isMarked(page, id) {
+    let results = await STORAGE.get(page + '\x1e' + id);
 
     return Object.keys(results).length != 0;
 }
 
-async function markComment(id) {
+async function markComment(page, id) {
     await STORAGE.set({
-        [id]: true
+        [page + '\x1e' + id]: true
     });
 }
 
-async function unmarkComment(id) {
-    await STORAGE.remove(id);
+async function unmarkComment(page, id) {
+    await STORAGE.remove(page + '\x1e' + id);
 }
 
 browser.menus.create({
@@ -41,6 +51,10 @@ browser.menus.create({
 
 // XXX handle promise failure?
 browser.menus.onClicked.addListener(async function(info) {
+    let currentPageId = extractPageId(info.pageUrl);
+    if(!currentPageId) {
+        return;
+    }
     let target = info.linkUrl;
     let id = extractCommentId(target);
 
@@ -48,9 +62,9 @@ browser.menus.onClicked.addListener(async function(info) {
         return;
     }
 
-    if(await isMarked(id)) {
-        await unmarkComment(id);
+    if(await isMarked(currentPageId, id)) {
+        await unmarkComment(currentPageId, id);
     } else {
-        await markComment(id);
+        await markComment(currentPageId, id);
     }
 });
--- content-script.js
+++ content-script.js
@@ -1,7 +1,22 @@
+const HN_PAGE_RE = new RegExp('^http[s]?://news[.]ycombinator[.]com/item[?]id=(\\d+)$');
 const MARKED_CSS_CLASS = 'marked-comment';
+
+function extractPageId(url) {
+    let match = HN_PAGE_RE.exec(url);
+
+    if(match) {
+        return match[1];
+    }
+
+    return null;
+}
 
 function refreshCommentMark(id, marked) {
     let commentElement = document.getElementById(id);
+
+    if(!commentElement) {
+        return;
+    }
 
     // XXX make sure the current classList is sane wrt. this state?
     if(marked) {
@@ -12,13 +27,23 @@ function refreshCommentMark(id, marked) {
 }
 
 browser.storage.onChanged.addListener(function(changes) {
-    for(let id of Object.keys(changes)) {
-        refreshCommentMark(id, changes[id].newValue);
+    let thisPageId = extractPageId(document.location);
+
+    for(let key of Object.keys(changes)) {
+        let [ pageId, id ] = key.split('\x1e');
+        if(pageId == thisPageId) {
+            refreshCommentMark(id, changes[key].newValue);
+        }
     }
 });
 
 browser.storage.local.get().then(function(marks) {
-    for(let id of Object.keys(marks)) {
-        refreshCommentMark(id, true);
+    let thisPageId = extractPageId(document.location);
+
+    for(let key of Object.keys(marks)) {
+        let [ pageId, id ] = key.split('\x1e');
+        if(pageId == thisPageId) {
+            refreshCommentMark(id, true);
+        }
     }
 });
